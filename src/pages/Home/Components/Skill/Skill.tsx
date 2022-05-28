25a08293d96c13c40b7678b1c3791f9fd9f9b56a
nitskp <nitinkpandey4@gmail.com>
2022-05-28 19:38:08 +0530

    removed eslint errors

--- src/pages/Home/Components/Skill/Skill.tsx
+++ src/pages/Home/Components/Skill/Skill.tsx
@@ -1,32 +1,34 @@
-
 interface Props {
   icon: React.ReactNode;
   name: string;
 }
 
-interface StylesObjectType {
-  diamondBorder: React.CSSProperties;
-  container: React.CSSProperties;
-}
+// interface StylesObjectType {
+//   diamondBorder: React.CSSProperties;
+//   container: React.CSSProperties;
+// }
 
 const Skill = ({ icon, name }: Props) => {
-  const styles: StylesObjectType = {
-    diamondBorder: {
-      rotate: "45deg",
-      border: `solid 2px rgba(210, 215, 217, 0.75)`,
-      borderRadius: `10px`,
-      width: `132px`,
-      height: '132px',
-     translate:`translateY(66px)`
-    },
-    container: {
-      border: `2px solid red`,
-      maxWidth: `fit-content`
-    }
-  };
+  // const styles: StylesObjectType = {
+  //   diamondBorder: {
+  //     rotate: "45deg",
+  //     border: `solid 2px rgba(210, 215, 217, 0.75)`,
+  //     borderRadius: `10px`,
+  //     width: `132px`,
+  //     height: '132px',
+  //    translate:`translateY(66px)`
+  //   },
+  //   container: {
+  //     border: `2px solid red`,
+  //     maxWidth: `fit-content`
+  //   }
+  // };
 
   return (
-    <></>
+    <>
+      {icon}
+      {name}
+    </>
   );
 };
 
